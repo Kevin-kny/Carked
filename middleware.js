51fcf15df2611fac3bdb006b40804ab6468a05c6
Kevin-kny <kevinkinyanjui2005@gmail.com>
2026-03-04 22:14:04 +0300

    Simpler small middleware

--- middleware.js
+++ middleware.js
@@ -1,6 +1,5 @@
-//import arcjet, { createMiddleware,shield,detectBot } from '@arcjet/next';
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
-import { NextResponse } from 'next/server';
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher([
   "/admin(.*)",
@@ -8,33 +7,16 @@ const isProtectedRoute = createRouteMatcher([
   "/reservations(.*)",
 ]);
 
-/*const aj = arcjet({ 
-  key: process.env.ARCJET_KEY, 
-  rules: [ 
-    //shield({ mode: "LIVE", }), 
-    //detectBot({ mode: "LIVE", allow: ["CATEGORY:SEARCH_ENGINE"], }), 
-    ] 
-  });*/
+export default clerkMiddleware(async (auth, req) => {
+  const { userId } = await auth();
 
-const clerk = clerkMiddleware(async (auth,req)=>{
-  const {userId}= await auth();
-
-  if(!userId && isProtectedRoute(req)){
-    const {redirectToSignIn}= await auth();
-    return redirectToSignIn();
+  if (!userId && isProtectedRoute(req)) {
+    return (await auth()).redirectToSignIn();
   }
 
   return NextResponse.next();
 });
 
-//export default createMiddleware(aj, clerk);
-export default clerk;
-
 export const config = {
-  matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
-    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
-    '/(api|trpc)(.*)',
-  ],
+  matcher: ["/admin(.*)", "/saved-cars(.*)", "/reservations(.*)"]
 };
